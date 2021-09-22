73f3f05b2cbcaca05a16e440d107e0f64e3a28bb
Carlo de Franchis <carlodef@gmail.com>
2021-09-22 13:15:20 +0200

    update version number to 1.0.4

--- setup.py
+++ setup.py
@@ -38,7 +38,7 @@ requirements = ['numpy']
 extras_require = {'test': ['pytest']}
 
 setup(name="ransac",
-      version="1.0.3",
+      version="1.0.4",
       description="Python wrapper of Enric Meinhardt's RANSAC implementation",
       long_description=readme(),
       long_description_content_type="text/markdown",
